3b3d5e0ba05858df9e4bc0556c25ca4489390829
Ayush-Kumar-Mohanty <ag2009gamer@gmail.com>
2026-03-08 20:19:02 +1100

    Create Learning_Notes.py

---
+++ Learning_Notes.py
@@ -0,0 +1,35 @@
+python
+# Day 1: Learning Python Basics .
+# I am learing this on youtube through (Python for Beginners - Learn Coding with Python in 1 Hour) .
+# I learn that a STRING means a sequence of characters .
+# I learn that we use variable to temporarily store data in a computers memory .
+# AND FIRST CODE I LEARN IS : 
+# name = input ('What is Your Name? ')
+# print ("hello there " + name)
+# age = input("What is your Age ? ")
+# print("Ok so your age is " + age)
+# birth_year = input("what is you bith year")
+# old = 2026 - int(birth_year)
+# print (old)
+# The best thing i ever learn I enjoyed it .
+# I TRY THIS CODE AND MADEMYFIRST CALCULATOR THAT ONLY SUMS INTEGERS:
+#print("hello there")
+#First_Number = input("First Number ")
+#Second_Number = input("Second Number ")
+#sum = int(First_Number ) + int(Second_Number )
+#print (sum)
+#I NEED TO KNOW WHAT WAS MY PROBLEM WHY CANT IT ADD INTEGERS WITH DECIMAL NUMBERS.
+#THEN I GOT This:
+#print("hello there")
+#First_Number = input("First Number: ")
+#Second_Number = input("Second Number: ")
+#sum = float(First_Number ) + float(Second_Number )
+#print ("sum: " + str(sum))
+#OR
+#print("hello there")
+#First_Number = float(input("First Number: "))
+#Second_Number = float(input("Second Number: "))
+#sum = First_Number + Second_Number
+#print ("sum: " + str(sum))
+#I THINK THIS IS HOW IT WORKS BUT STILL ADDING INTEGERS ON IT SHOWS A (INTEGER.0) AFTER THE SUM STILL I AM LERANING.
+#This is all for today i will update if i learn topday more as in part 2.
