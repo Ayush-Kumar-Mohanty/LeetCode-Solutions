89c606bf5d0b7b69b1c707d2778b3c4d05a0e9d5
Ayush-Kumar-Mohanty <ag2009gamer@gmail.com>
2026-03-12 04:16:29 +1100

    Create  Learning_Notes(2).py

---
+++ Learning_Notes(2).py
@@ -0,0 +1,15 @@
+#Today i learned about this:
+#i = 1
+#while i <= 10:
+#print (i)
+# i = i + 1
+#AND 
+#i = 1
+#while i <= 10:
+#print (i * "*")
+# i = i + 1
+#Also I Learned About lists:
+#camera = ["ayush", "yash", "yadav", "suvender"]
+#print (camera[0:2])
+#the above fuction will only give output of the names ayush,yash and yadav except suvender if i hasd sai8d only camera then--
+#-it would have showed me all names on the list.
